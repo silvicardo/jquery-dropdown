1fbe95a43f61fd53b2e89ddc3e9d7e4ad170598e
silvicardo <silvicardo@gmail.com>
2018-11-29 16:39:09 +0100

    first level dropdown

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 //dropdown.main.js
 
 console.log('Hello from main.js');
+
+$('.dropdown_link_area').mouseenter(function(){
+
+  console.log('ciao ho cliccato e non so scrivere codice');
+
+  $('.dropdown_menu').removeClass('active');
+
+  $(this).children('.dropdown_menu').addClass('active');
+
+});
+
+
+$('.dropdown_link_area').mouseleave(function(){
+  $('.dropdown_menu').removeClass('active');
+}
+)
